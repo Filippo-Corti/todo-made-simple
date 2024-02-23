48b2fb079a14d026ba31f31647e7434d2dab13ff
Filippo <corti.filippo03@gmail.com>
2024-02-23 22:31:14 +0100

    Actually working??

--- extension.js
+++ extension.js
@@ -5,6 +5,12 @@ const vscode = require('vscode');
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 
+const UNCHECKED = "☐";
+const CHECKED = "☑";
+
+const COMMAND = "!TODO";
+
+
 /**
  * @param {vscode.ExtensionContext} context
  */
@@ -17,40 +23,104 @@ function activate(context) {
 			return;
 		}
 
-		vscode.window.showInformationMessage('Testing from TODO Made Simple!');
+		editor.edit((editBuilder) => {
+			//Add Checkbox
+			editBuilder.insert(editor.selection.active, UNCHECKED + " ");
+		}).then(() => {
+			//Add Tooltip
 
-		// Create a decoration type
-		const decorationType = vscode.window.createTextEditorDecorationType({
-			gutterIconPath: context.asAbsolutePath('unchecked.svg'),
-			gutterIconSize: '16px',
-			overviewRulerLane: vscode.OverviewRulerLane.Center,
-			rangeBehavior: 1, //Closed, Closed
-		});
+			// Define a range where you want to insert the clickable text
+			const range = new vscode.Range(editor.selection.active.translate(0, -2), editor.selection.active.translate(0, -1));
+			const decoration = {
+				range: range,
+				hoverMessage: 'Click to check', // Optional hover message
+			};
+			const decorationTypePopup = vscode.window.createTextEditorDecorationType({
+				rangeBehavior: 1, //Closed, Closed
+				cursor: 'pointer',
+			});
+			editor.setDecorations(decorationTypePopup, [decoration]);
+		})
+	});
 
-		// Define a range where you want to insert the clickable text
-		const range = new vscode.Range(editor.selection.active, editor.selection.active);
+	//Handle clicks
+	let toggleCheckboxes = vscode.window.onDidChangeTextEditorSelection(event => {
+		if (event.textEditor !== vscode.window.activeTextEditor) {
+			return;
+		}
+		const activePosition = event.selections[0].active;
+		switch (event.kind) {
+			case 1: //Inserting text
+				const rangeForTODOText = new vscode.Range(activePosition.translate(0, - COMMAND.length), activePosition);
+				const text = event.textEditor.document.getText(rangeForTODOText);
+				if (text == COMMAND) {
+					event.textEditor.edit((editBuilder) => {
+						//Add Checkbox
+						editBuilder.replace(rangeForTODOText, "");
+					}).then(() => {
+						vscode.commands.executeCommand('todo-made-simple.addCheckItem');
+					})
+				}
+				break;
+			case 2: //Selecting text
+				//Try checking the right side of the cursor
+				try {
+					const rangeRight = new vscode.Range(activePosition, activePosition.translate(0, 1));
+					const charOnTheRight = event.textEditor.document.getText(rangeRight);
+					replaceChar(rangeRight, charOnTheRight);
+				} catch (e) { }
+				//Try checking the left side of the cursor
+				try {
+					const rangeLeft = new vscode.Range(activePosition.translate(0, -1), activePosition);
+					const charOnTheLeft = event.textEditor.document.getText(rangeLeft);
+					replaceChar(rangeLeft, charOnTheLeft);
+				} catch (e) { }
+				break;
+			default:
+				break;
+		}
+	});
+
+
+	/**
+	 * 
+	 * @param {vscode.Range} range
+	 * @param {string} char
+	 */
+	function replaceChar(range, char) {
+		const editor = vscode.window.activeTextEditor;
+		const decorationRange = editor.document.lineAt(range.start).range;
 
 		const decoration = {
-			range: range,
-			hoverMessage: 'Click to check', // Optional hover message
+			range: decorationRange,
 		};
 
-		editor.setDecorations(decorationType, [decoration]);
-
-		// Handle click events on gutter icons
-		let disposable = vscode.window.onDidChangeTextEditorSelection(event => {
-			if (event.textEditor === vscode.window.activeTextEditor) {
-				const cursorPosition = event.selections[0].active;
-				const line = cursorPosition.line;
-				if (line === range.start.line) {
-					vscode.window.showInformationMessage('You clicked the gutter icon!');
-					// Add your desired functionality here
-				}
-			}
+		//Empty but editable if wanted
+		const decorationTypeChecked = vscode.window.createTextEditorDecorationType({
 		});
 
-		context.subscriptions.push(disposable);
-	})
+		const decorationTypeUnchecked = vscode.window.createTextEditorDecorationType({
+		});
+
+		if (char == UNCHECKED) {
+			//Check the box and add decoration
+			editor.edit((editBuilder) => {
+				editBuilder.replace(range, CHECKED);
+			}).then(() => {
+				editor.setDecorations(decorationTypeChecked, [decoration]);
+			});
+		} else if (char == CHECKED) {
+			//Uncheck the box and remove the decoration
+			editor.edit((editBuilder) => {
+				editBuilder.replace(range, UNCHECKED);
+			}).then(() => {
+				editor.setDecorations(decorationTypeUnchecked, [decoration]);
+			});
+		}
+	}
+
+	context.subscriptions.push(toggleCheckboxes);
+	context.subscriptions.push(addCheckItem);
 }
 
 // Handle cursor position changes
